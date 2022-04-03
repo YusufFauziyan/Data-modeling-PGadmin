a35c96ba61f42c42e6afc9628a6a1fcaef36e6a9
yusuf fauziyan <yusuffauziyan@gmail.com>
2022-04-03 13:42:11 +0700

    commit all file

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const db = require('./connection/db')
+
+// set view engine hbs
+app.set ('view engine', 'hbs')
+// set public path/folder
+app.use('/public', express.static(__dirname + '/public')) 
+
+app.use(express.urlencoded({extended: false}))
+
+
+app.get ('/', (req, res) => {
+
+        db.connect((err, client, done) => {
+        if (err) throw err
+
+        client.query('SELECT * FROM tb_projects', (err, result) => {
+            if (err) throw err
+            let data = result.rows
+
+            // data = data.map(function (item) {
+            //     return {
+            //         ...item,
+            //         projectName: item.name,
+            //         description: item.description.slice(0, 150) + '.....',
+            //         sDate: item.sDate,
+            //         eDate: item.eDate,
+            //         duration: abtDuration(item.startDate, item.eDate),
+            //         react: item.react,
+            //         nextjs: item.nextjs,
+            //         javascript: item.javascript,
+            //         vuejs: item.vuejs
+            //     }
+            // })
+            
+            res.render('index', { blogs: data })
+        })
+    })
+})
+
+app.get ('/contact', (req, res) =>{
+    res.render('contact', {title: 'Halaman contact'})
+})
+
+app.get ('/add-project', (req, res) =>{
+
+    res.render('add-project', {title: 'Halaman AddProject'})
+})
+
+app.get ('/blog', (req, res) =>{
+    res.render('blog', {title: 'Halaman Blog'})
+})
+
+app.get ('/update-project', (req, res) =>{
+    res.render('update-project', {title: 'Halaman Project'})
+})
+
+app.get ('/add-my-project', (req, res) =>{
+    res.render('add-my-project', {title: 'Halaman AddMyProject'})
+})
+
+app.use ('/', (req, res) => {
+    res.status(404)
+    res.send('<h1>404</h1>')
+})
+
+function abtDuration(startDate, endDate) {
+    let start = new Date(startDate);
+    let end = new Date(endDate);
+    let duration = end.getTime() - start.getTime();
+    let year = Math.floor(duration / (1000 * 3600 * 24 * 30 * 12))
+    let month = Math.round(duration / (1000 * 3600 * 24 * 30));
+    let day = duration / (1000 * 3600 * 24)
+  
+    if (day < 30) {
+        return day + ' Hari';
+    } else if (month < 12) {
+        return month + ' Bulan';
+    } else {
+        return year + ' Tahun'
+    }
+
+}
+
+app.listen(port, () =>{
+    console.log(`Server listen at http://localhost: ${port}`)
+})
